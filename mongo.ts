bf289cd276f14a0858be58de05a521687c942ed1
Abbas Lamouri <abbaslamouri@yrlus.com>
2022-06-30 07:39:11 -0400

    M

--- mongo.ts
+++ mongo.ts
@@ -3,8 +3,6 @@ import { MongoClient } from 'mongodb'
 // const config = useRuntimeConfig()
 // console.log('CCCCC', config)
 
-const client = new MongoClient(
-  'mongodb+srv://doadmin:316kz9WZU74rf8G5@lightmylamp-6d2bbfe9.mongo.ondigitalocean.com/lightmylamp?tls=true&authSource=admin&replicaSet=lightmylamp'
-)
+const client = new MongoClient('')
 
 export default client
